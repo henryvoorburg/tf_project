c7f0f83a57e292aef5ab33d237e459a2f4a02ee7
G G <x.x@gmail.com>
2020-01-09 12:27:35 +0100

    Fixtures maken gebruik van EntityManager i.p.v references. Deelnemerlijst overzicht heeft nu knoppen.

--- src/DataFixtures/InstructorFixtures.php
+++ src/DataFixtures/InstructorFixtures.php
@@ -5,20 +5,28 @@ use App\Entity\Person;
 use App\Repository\InstructorRepository;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Faker\Provider\nl_NL as Nep;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManager as em;
 class InstructorFixtures extends BaseFixture implements OrderedFixtureInterface
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(Instructor::Class, 1, function (Instructor $instructor, $count) {
-            $personRepo = $this->referenceRepository->getManager()->getRepository(Person::class);
+            $personRepo = $this->em->getRepository(Person::class);
+            $rand = $this->faker->unique()->numberBetween(0, count( $personRepo->findAll() ));
             $instructor
-                ->setPerson($personRepo->findOneBy(['id' => $this->faker->unique()->numberBetween(1, count( $personRepo->findAll() ))]))
+                ->setPerson($personRepo->findOneBy(['id' => $rand]))
                 ->setSalary(2000)
                 ->setHiringDate($this->faker->dateTime());
+            $personRepo->findOneBy(['id' => $rand])->setRoles(['ROLE_INSTRUCTOR']);
         });
         $manager->flush();
     }
--- src/DataFixtures/MemberFixtures.php
+++ src/DataFixtures/MemberFixtures.php
@@ -9,17 +9,22 @@ use App\Entity\Person;
 use App\Entity\Training;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 
 class MemberFixtures extends BaseFixture implements OrderedFixtureInterface
 {
-
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(Member::Class, 5, function (Member $member, $count) {
-            $personRepo = $this->referenceRepository->getManager()->getRepository(Person::class);
+            $personRepo = $this->em->getRepository(Person::class);
             $member
-                ->setPerson($personRepo->findOneBy(['id' => $count]))
+                ->setPerson($personRepo->findOneBy(['id' => $count+1]))
                 ->setStreet($this->faker->streetAddress)
                 ->setPlace($this->faker->city)
                 ->setPostalCode($this->faker->postcode);
